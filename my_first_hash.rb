24f78fa1d1a3ac7cd64f501bff676170ca11c131
Corey Martin <coreyjmartin2@gmail.com>
2020-08-29 19:05:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -39,7 +39,7 @@ def adding
                  
   shipping_manifest["muskets"] = 2 
   
-  shipping_manifest["gun powder"] = 2
+  shipping_manifest["gun powder"] = 4
   # add 2 muskets to the shipping_manifest hash below
 
 
